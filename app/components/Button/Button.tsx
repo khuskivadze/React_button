098ff25b9a67e346ebc1f3358368481f64410d6d
thamar khuskivadze <thamarkhuskivadze@thamars-Laptop.local>
2024-01-19 18:38:17 +0400

    add new tasks

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,15 +1,19 @@
 import styles  from './Button.module.scss'
+import { TbUserCheck } from "react-icons/tb";
 
 type  Props = {
     title  : string;
     className? : string;
     desibled?: boolean;
-    mode? :  'fill' | 'outline';
+    mode? :  'fill' | 'outline' | 'warring' | 'success' | 'error' | 'information'
+    | 'lighWarring' | 'lightSuccess' | 'lightError' | 'lifghtInformation';
+    icon? : 'on' | 'off';
 }
 
 export  default (props : Props) => {
 
-    const  classes =  [styles.btContainer, props.className]    
+    const  classes =  [styles.btContainer, props.className, styles.button] ;
+    const iconClass = [props.className];  
     // join - ერეკიში ჩაშენებული მეთოფი რომელიც ერეის 
     //ყველა ელემენტს აიღებს, ერთ სტრინგში ჩაყრის  და მათ 
     //შორის მოათავებს გამოტოვებას;  
@@ -18,15 +22,43 @@ export  default (props : Props) => {
 
     if(props.mode == 'outline') {
         classes.push(styles.outline)
-    } else {
+    }else if(props.mode == 'success'){
+        classes.push(styles.success)
+    }else if(props.mode == 'warring'){
+        classes.push(styles.warring)
+    }else if(props.mode == 'error'){
+        classes.push(styles.error)
+    }else if(props.mode == 'information'){
+        classes.push(styles.information)
+    }
+    else if(props.mode == 'lightSuccess'){
+        classes.push(styles.lightSuccess)
+    }else if(props.mode == 'lighWarring'){
+        classes.push(styles.lighWarring)
+    }else if(props.mode == 'lightError'){
+        classes.push(styles.lightError)
+    }else if(props.mode == 'lifghtInformation'){
+        classes.push(styles.lifghtInformation)
+    }else {
         classes.push(styles.fill)
+    }
+
+
+
+    if(props.icon == 'off') {
+        iconClass.push(styles.icon_off )
+    }else{
+      
+         iconClass.push(styles.icon_on)
     }
 
     
     return (
-        <button disabled={props.desibled} 
-           className={classes.join(' ').trim()}>
+        <button disabled={props.desibled } 
+         className={classes.join(' ').trim()}>
+            <TbUserCheck  className= {iconClass.join(' ').trim()}/>
             {props.title}
+            
         </button>
         
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Button from './components/Button/Button'
+import Link from 'next/link'
+ 
+ 
 
 export default function Home() {
   return (
     <div className= {styles.mainPage}>
-        
-      <Button mode='fill'  title='რეგისტრაცია'   />
+       <Button   mode='fill' icon='off'  title='რეგისტრაცია' />
+      
     </div>
   )
 }
